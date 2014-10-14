84fae0308999237a51ea62d16162138e6ce7699d
croshelli <roshellikatie33@gmail.com>
2014-10-14 15:15:17 -0400

    Bike position fixed

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,17 @@ $(document).ready(function logoAnimation() {
     	$(this).animate({rotate:'+=7deg'},1500);
     });
 });
+
+// /// BEGIN - V ALIGN /// //
+
+$(document).ready(function bikeBind(){        
+    var $bike_mask = $('#bike_mask');
+	$(window).bind('scroll', function() {
+		$bike_mask.css('top', 576-$(window).scrollTop() + 'px');
+		// $(window).stopTime('initiate_bridge_fade').oneTime(100, 'initiate_bridge_fade', function() {
+		// 	animateBridge( $(window).scrollLeft() > 1400 );
+		});
+	});
+   
+
+// /// END - V ALIGN /// //
