f24cb01756fd4a164ecb1308aef2ff216edf8a2d
croshelli <roshellikatie33@gmail.com>
2014-10-14 15:42:32 -0400

    creating bike wrappers for keyboard

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+//2014 futureofcarsharing.com
+
 $(document).ready(function logoAnimation() {
     $("#soma-logo-kids").bind('mouseover', function(){
     	$(this).animate({rotate:'+=7deg'},1500);
@@ -10,10 +12,53 @@ $(document).ready(function bikeBind(){
     var $bike_mask = $('#bike_mask');
 	$(window).bind('scroll', function() {
 		$bike_mask.css('top', 576-$(window).scrollTop() + 'px');
-		// $(window).stopTime('initiate_bridge_fade').oneTime(100, 'initiate_bridge_fade', function() {
-		// 	animateBridge( $(window).scrollLeft() > 1400 );
 		});
 	});
    
 
 // /// END - V ALIGN /// //
+
+// CAR BUBBLE CONTENT ANIMATION
+$(document).ready(function bikeBubbleAnimation() {
+    
+    $(document).delay(6000),
+    $("#bike-bubble-text").
+      animate({opacity:1},400,'linear').
+      delay(1600).
+      animate({opacity:0},400,'linear'),
+    $("#bike-keyboard").
+      delay(2400).
+      animate({opacity:1},400,'linear').
+      delay(2400).
+      animate({opacity:0},400,'linear',bikeBubbleAnimation);     
+});
+
+// CAR KEYBOARD ANIMATION
+$(document).ready(function bikeKeyboardAnimation() {
+    $("#bike-keyboard-left").
+      animate({opacity:1},100,'linear').
+      animate({opacity:0},100,'linear'),
+    $("#bike-keyboard-right").
+      delay(200).
+      animate({opacity:1},100,'linear').
+      animate({opacity:0},100,'linear',bikeKeyboardAnimation);   
+});
+
+
+
+// /// BEGIN - EXECUTE SCROLL ANIMATIONS /// //
+
+$(document).ready(function scrollAnimations(){
+	
+	// SCROLL CAR BUBBLE
+    $(document).ready(function() {
+	    $(window).bind('scroll', function() {
+		    scrollAnimationBikeBubble( $(window).scrollLeft() > 600 );
+	    });    
+        function scrollAnimationCarBubble() {
+            if( $("#bike-bubble-wrapper").css('opacity') == 1) 
+        	$("#bike-bubble-wrapper").animate({ opacity:0},200);
+        }
+    });
+
+    });
