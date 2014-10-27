43d455ba8f727cc04222774957d86525a1a83a99
jshmtthwclrk <jshmtthwclrk@Schwas-rMBP-2.local>
2014-10-26 21:59:52 -0400

    Navigation Bar
    javascript was not working correctly.

--- javascript/script.js
+++ javascript/script.js
@@ -85,23 +85,4 @@ $(document).ready(function scrollAnimations(){
         }
     });
 
-    });
-
-// /// NAVIGATION HORZONTIAL SCROLLING EFFECT /// //
 /* http://tympanus.net/codrops/2010/06/02/smooth-vertical-or-horizontal-page-scrolling-with-jquery/ */
-
-$(function() {
-    $('div.navigation a').bind('click',function(event){
-        var $anchor = $(this);
-        /*
-        if you want to use one of the easing effects:
-        $('html, body').stop().animate({
-            scrollLeft: $($anchor.attr('href')).offset().left
-        }, 1500,'easeInOutExpo');
-         */
-        $('html, body').stop().animate({
-            scrollLeft: $($anchor.attr('href')).offset().left
-        }, 1000);
-        event.preventDefault();
-    });
-});
